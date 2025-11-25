8eb3a89473d0095a6e031bb819cccbd5ac2cfeb8
fasil916 <71936145+fasil916@users.noreply.github.com>
2025-11-25 13:46:37 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World from Java Maven Project2!");
+        System.out.println("Hello World from Java Maven Project3!");
     }
 }

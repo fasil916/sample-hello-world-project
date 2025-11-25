311c395868090dc06c610febc62a5cbadd0733a6
fasil916 <71936145+fasil916@users.noreply.github.com>
2025-11-25 11:50:52 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,8 +1,7 @@
-package com.demo;
+package com.example;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World from Java Maven Project!");
     }
 }
-
